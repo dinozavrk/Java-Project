9b475cda7afd978f0ad65fb8ce0728bf9fc17116
Стас <pamurzinstas@gmail.com>
2025-12-05 13:10:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const phrases = [
+    "отправлю другу смешную гифку",
+    "посмотрю скидки на авиасейлс",
+    "разберусь, о чем поют реперы",
+    "расставлю книги на полке по цветам",
+    "посмотрю на смешных лягушек",
+];
+
+console.log(phrases.length)
+console.log(phrases[0])
+
+console.log(phrases[phrases.length - 1])
+
+// let rand = Math.random() * 10
+// console.log(rand)
+
+// console.log(Math.pow(2,10))
+// console.log(Math.sqrt(16))
+
