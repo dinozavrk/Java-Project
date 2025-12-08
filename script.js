9e18f13ac688c43218d0095310628e9906355c72
Стас <pamurzinstas@gmail.com>
2025-12-08 15:37:44 +0300

    four

--- script.js
+++ script.js
@@ -6,18 +6,71 @@ const phrases = [
     "посмотри на рекролл",
     "а ты шаришь за 42?",
     "иди учись",
-];
+]; //Math.pow(x,y)
 
-const newArr = [1, 2, 3, 4, 52]
+// const newArr = [1, 2, 3, 4, 52]
 
 const getRandomElement = (arr) => {
     const randomIndex = Math.floor(Math.random() * arr.length)
-    console.log(arr[randomIndex])
-
+    // console.log(arr[randomIndex])
+    return arr[randomIndex]
 }
-getRandomElement(phrases)
+let randomElement = getRandomElement(phrases)
+console.log(randomElement)
 
-const res = getRandomElement()
+const btn1 = document.querySelector(".btn1")
+console.log(btn1)
+const image = document.querySelector(".image")
+console.log(image)
+const advice = document.querySelector(".advice")
+console.log(advice)
+const advice__paragraph = document.querySelector(".adviceparagraph")
+console.log(advice__paragraph)
+
+btn1.addEventListener("click", () => {
+    console.log("52")
+})
+
+// let x = prompt("Введите число")
+// let y = prompt("Введите вашу степень")
+
+// const pow = (x, y) => {
+
+//     let n = x**y
+
+//     if( x === null || x.trim() === "" ) 
+//         return"ВВЕДИ ЧИСЛО"
+//     if( y === null || y.trim() === "")
+//         return"ВВЕДИ ЧИСЛО"
+//     if(isNaN(x) || isNaN(y))
+//         return"НУЖНО ЧИСЛО!!!!"
+
+//     return n
+
+//     }
+
+// console.log(pow(x, y))
+
+
+
+// let x = prompt("Введите число")
+// let y = prompt("Введите вашу степень")
+
+// const pow = (x, y) => {
+
+//     let n = x**y
+
+//     if(!x || !y)
+//         return"ВВЕДИ ТЫ УЖЕ ЧИСЛО"
+//     if(isNaN(x) || isNaN(y))
+//         return"ТЫ МНЕ УЖЕ НАДОЕЛ, ВВЕДИ ЧИСЛА"
+//     return n
+
+//     }
+
+// console.log(pow(x, y))
+
+
 
 
 
