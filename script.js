f1ac30e6757f38ae3510ba0233e29a49e47224e9
Стас <pamurzinstas@gmail.com>
2025-12-08 13:24:41 +0300

    third

--- script.js
+++ script.js
@@ -8,32 +8,91 @@ const phrases = [
     "иди учись",
 ];
 
-// console.log(phrases.length)
-// console.log(phrases[0])
+const newArr = [1, 2, 3, 4, 52]
 
-// console.log(phrases[phrases.length - 1])
+const getRandomElement = (arr) => {
+    const randomIndex = Math.floor(Math.random() * arr.length)
+    console.log(arr[randomIndex])
 
-// let rand = Math.random() * 10
-// console.log(rand)
-
-// console.log(Math.pow(2,10))
-// console.log(Math.sqrt(16))
-// console.log(Math.floor(Math.random() * 10))
-
-// const randomIndex = Math.floor(Math.random() * phrases.length)
-
-// console.log(phrases[randomIndex] )
-
-const min = Math.min()
-const max = Math.max()
-function randomInteger(min, max){
-    
-    return Math.floor(Math.random() * (max - min + 1) + min)
-    
 }
-console.log(randomInteger(1, 5))
-console.log(randomInteger(7, 15))
-console.log(randomInteger(40, 42))
+getRandomElement(phrases)
+
+const res = getRandomElement()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const min = Math.min()
+// const max = Math.max()
+// function randomInteger(min, max){ 
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+// }
+// console.log(randomInteger(1, 5))
+// console.log(randomInteger(7, 15))
+// console.log(randomInteger(40, 42))
+
+
 
 
 
